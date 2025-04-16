4cf22f96ad30ab54f7258a30e3bbf2e4cf6b054b
frotaana <frotaana2005@gmail.com>
2025-04-16 01:00:29 -0400

    landing page

---
+++ assets/script/script.js
@@ -0,0 +1,6 @@
+let botao;
+botao = document.getElementById("conteudo-principal-escrito-botao")
+
+botao.addEventListener("click",()=>{
+    window.open("https://www.linkedin.com/in/ana-b-74a264254/", "_blank");
+})
