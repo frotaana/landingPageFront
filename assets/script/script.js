92f7c0b5a00ad0f18dcf345a924d42e09b6bea78
frotaana <frotaana2005@gmail.com>
2025-04-16 01:10:50 -0400

    troca de botões: LinkedIn, Git Hub

--- assets/script/script.js
+++ assets/script/script.js
@@ -2,5 +2,5 @@ let botao;
 botao = document.getElementById("conteudo-principal-escrito-botao")
 
 botao.addEventListener("click",()=>{
-    window.open("https://www.linkedin.com/in/ana-b-74a264254/", "_blank");
+    window.open("https://github.com/frotaana", "_blank");
 })
